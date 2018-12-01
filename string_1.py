a113278b6df6ccf579bb6039927da5ebb556a25d
PaponAshraf <33818732+PaponAshraf@users.noreply.github.com>
2018-12-01 23:13:52 +0600

    Add files via upload

---
+++ string_1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#line_1="Bangla"
+#line_2="desh"
+#line=line_1+line_2
+#print(line)#this is a comment
+#newline=line_1.swapcase()
+#print(newline)
+#
+#print(type(True))
+
+
+
+#list=['a','e','i','o','u']
+#x='y'
+#answer=x in list
+#
+#if answer==True:
+#    print ('paisi')
+#else:
+#    print ('hayre pai nai')
+
+#i=1
+#sum=0
+#
+#while i<=10:
+#    print (i)
+#    sum=sum+i
+#    i=i+1
+#print("sum=")
+#print(sum)
+
+
+#fib_1=0
+#fib_2=1
+#
+#while fib_2<1000000:
+#    print (fib_2,)
+#    fib_1 , fib_2 = fib_2 , fib_1 + fib_2
+
+range()
